e90bccfdd91e0af6da562819732a91271b458925
Deepak <deepaksubamane@gmail.com>
2019-11-11 20:35:23 +0530

    minor changes

--- app/src/main/java/com/example/testapp/network/RetrofitFactory.java
+++ app/src/main/java/com/example/testapp/network/RetrofitFactory.java
@@ -1,5 +1,7 @@
 package com.example.testapp.network;
 
+import android.util.Log;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -15,7 +17,7 @@ public class RetrofitFactory {
 
     }
 
-    public static Retrofit getRetrofit() {
+    public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl("https://bank-app-test.herokuapp.com")
